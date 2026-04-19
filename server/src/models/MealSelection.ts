25fa55376664b94858436fb92d69bb5c3bb988bc
ALD2N <axel14022001@gmail.com>
2026-04-19 19:26:47 +0200

    feat: create MealSelection model

---
+++ server/src/models/MealSelection.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { IMealSelection } from '@dndmeal/shared';
+
+export interface IMealSelectionDocument extends IMealSelection, Document {}
+
+const mealSelectionSchema = new Schema<IMealSelectionDocument>(
+  {
+    recipe: {
+      type: Schema.Types.ObjectId,
+      ref: 'Recipe',
+      required: true,
+    },
+    selectedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Index to find current meal quickly
+mealSelectionSchema.index({ date: -1 });
+
+export const MealSelection = mongoose.model<IMealSelectionDocument>(
+  'MealSelection',
+  mealSelectionSchema
+);
